1c166d1a70a2468a255cd7b5ed91906782b71481
Anik Sarker <ach6266@gmail.com>
2022-10-17 02:41:52 +0600

    Map Integrated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
+import { Map, NavigationControl, Marker, NavigationOptions } from 'maplibre-gl';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+  map: Map | undefined;
 
-  constructor() { }
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+    const key = 'SoL71Zyf7SmLrVYWC7fQ';
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/eef16200-c4cc-4285-9370-c71ca24bb42d/style.json?key=CH1cYDfxBV9ZBu1lHGqh`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
+    });
+    this.map.addControl(
+      new NavigationControl({
+        showCompass: true,
+        showZoom: true,
+        visualizePitch: true,
+      }),
+      'top-right'
+    );
+    new Marker({ color: '#FF0000' })
+      .setLngLat([139.7525, 35.6846])
+      .addTo(this.map);
   }
 
+  ngOnDestroy() {
+    this.map?.remove();
+  }
 }
