a756c0f8656599fea91a3ec46a9d1ce8168eb1a5
Anik Sarker <ach6266@gmail.com>
2022-10-17 04:44:38 +0600

    all the map features with additional features are integrated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,14 @@ import {
   AfterViewInit,
   OnDestroy,
 } from '@angular/core';
-import { Map, NavigationControl, Marker, NavigationOptions } from 'maplibre-gl';
+import {
+  Map,
+  NavigationControl,
+  Marker,
+  GeolocateControl,
+  Popup,
+  LngLatBounds,
+} from 'maplibre-gl';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +21,8 @@ import { Map, NavigationControl, Marker, NavigationOptions } from 'maplibre-gl';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
-  map: Map | undefined;
-
+  map!: Map;
+  markers: Marker[] = [];
   @ViewChild('map')
   private mapContainer!: ElementRef<HTMLElement>;
 
@@ -31,6 +38,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       style: `https://api.maptiler.com/maps/eef16200-c4cc-4285-9370-c71ca24bb42d/style.json?key=CH1cYDfxBV9ZBu1lHGqh`,
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom,
+      doubleClickZoom: false,
     });
     this.map.addControl(
       new NavigationControl({
@@ -40,9 +48,58 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       }),
       'top-right'
     );
-    new Marker({ color: '#FF0000' })
-      .setLngLat([139.7525, 35.6846])
-      .addTo(this.map);
+
+    //move to your location
+    // var geolocate = new GeolocateControl({
+    //   positionOptions: {
+    //     enableHighAccuracy: true,
+    //   },
+    //   trackUserLocation: true,
+    // });
+    // // Add the control to the map.
+    // this.map.addControl(geolocate);
+    // this.map.on('load', function () {
+    //   geolocate.trigger();
+    // });
+
+    //adding a new marker to the map
+    // new Marker({ color: '#FF0000', draggable: true })
+    //   .setLngLat([initialState.lng, initialState.lat])
+    //   .addTo(this.map);
+
+    //adding a new marker to the map on double click
+    // this.map.on('dblclick', (e) => {
+    //   // console.log('A click event has occurred at ' + e.lngLat); // coordinates of the point on the map that was clicked. // The event object (e) contains information like the
+    //   this.map.flyTo({
+    //     center: e.lngLat,
+    //     duration: 2000,
+    //     zoom: 15,
+    //   });
+    //   var marker = new Marker({ color: '#FF0000', draggable: true })
+    //     .setPopup(new Popup().setHTML('<h1>Clicked Location!</h1>'))
+    //     .setLngLat(e.lngLat)
+    //     .addTo(this.map);
+    //   marker._lngLat;
+    //   this.markers.push(marker);
+    // });
+    //Auto Zoom to center map on a group of pins.
+    // setTimeout(() => {
+    //   console.log('setTimeout');
+    //   if (this.markers.length > 1) {
+    //     var bounds = new LngLatBounds();
+    //     this.markers.forEach(function (feature) {
+    //       bounds.extend(feature.getLngLat());
+    //     });
+
+    //     this.map.fitBounds(bounds);
+    //   }
+    // }, 10000);
+
+    //removing all the marker
+    // this.map.on('dblclick', (e) => {
+    //   console.log('dblclick');
+    //   this.markers.forEach((marker) => marker.remove());
+    // });
   }
 
   ngOnDestroy() {
